358b46e46c9ef84c8d34136dadd31c6509cda1ee
abhay <abhayviswakarma800@gmail.com>
2024-11-04 22:00:17 +0530

    final_commit

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello Bhai");
-  }
-}
+    class Simple{
+        public static void main(String args[]) {
+         System.out.println("Hello Bhai welcome");
+        }
+    }
