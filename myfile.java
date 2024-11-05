01604d6c9fe2a1325d480e09977b15110e44f9ca
abhay <abhayviswakarma800@gmail.com>
2024-11-05 21:15:29 +0530

    weaker

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
     class Simple{
         public static void main(String args[]) {
-         System.out.println("Hell i am boy Welcome");
+         System.out.println("Hello, Welcome to India");
         }
     }
